9bedc13198639b329ec431e4e3ba39ca8784d9f6
unknown <>
2019-03-24 18:33:17 +0300

    Initial commit

---
+++ app/src/main/java/com/simcoder/uber/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.simcoder.uber;
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID="ARnJeS3Hv_yKiNObK1jRmKRAQT-bX1e3a0nvpKa1wTjJCON6tsqwIovo80vC4Gj8KZm2ctCGF_NjCugN";
+}
